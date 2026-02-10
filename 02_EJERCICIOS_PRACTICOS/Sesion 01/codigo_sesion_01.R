976a1a0eb2e9fd2d65511b2202aed43f99e91766
Jorge Juvenal Campos Ferreira <41249739+JuveCampos@users.noreply.github.com>
2026-02-10 12:53:49 -0600

    [20260210] Ajustes presentaciones y carga codigo sesion 1

---
+++ 02_EJERCICIOS_PRACTICOS/Sesion 01/codigo_sesion_01.R
@@ -0,0 +1,45 @@
+
+
+# Hola. Este es el código de la sesión 01. 
+# El primer código.
+
+# 1. Para instalar paquetes: 
+install.packages("plotly") # O usando el asistente de "Packages" que vimos en clase. 
+# Correr esta línea de código hará que instales forzosamente el paquete (aunque ya lo tengas)
+
+# usar como calculadora
+3+2 # Suma
+3*2 # Multiplicación 
+(3*2)/3 # Operaciones complejas
+
+# asignacion de variables 
+a<-3
+b<-4
+# En R (a diferencia de Python) el espacio no importa tanto
+    x    <-4
+
+# Funciones matemáticas 
+sqrt(9) # Raíz cuadrada
+abs(-5) # Valor absoluto
+round(3.7777773) # Redondeo 
+  
+# Importar librerías es similar al Python.   
+# import pandas as pd # En Python
+# library(pandas) # Si fuera pandas una librería de R. 
+
+# Acá estamos importando las librerías plotly y tidyverse
+library(plotly)
+library(tidyverse)
+
+# Escalar
+a = 10
+palabra <- "María"
+condicion <- TRUE
+
+# Vectores 
+vector_texto <- c("María", "José", "Jesus")
+vector_numerico <- c(1,2,3)
+vector_condicones <- c(TRUE, FALSE, FALSE)
+colores <- c("black", "white", "#971599", "#9215e6")
+
+
