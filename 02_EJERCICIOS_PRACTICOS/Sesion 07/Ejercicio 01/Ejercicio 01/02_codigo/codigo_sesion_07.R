9723146a247ae769aeaeb34ad073ab2f1ac97297
Jorge Juvenal Campos Ferreira <41249739+JuveCampos@users.noreply.github.com>
2026-03-06 00:12:09 -0600

    [REPO] Tarea 01

--- 02_EJERCICIOS_PRACTICOS/Sesion 07/Ejercicio 01/Ejercicio 01/02_codigo/codigo_sesion_07.R
+++ 02_EJERCICIOS_PRACTICOS/Sesion 07/Ejercicio 01/Ejercicio 01/02_codigo/codigo_sesion_07.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(sf)
 library(janitor)
 library(ggimage)
+library(ggthemes)
 
 # Cargamos los datos ----
 datos <- readRDS("01_datos/datos_atus_2023.rds") %>% 
@@ -38,7 +39,8 @@ datos_grafica_1 <- datos %>%
 
 datos_grafica_1
 
-grafica <- datos_grafica_1 %>% 
+grafica <-
+  datos_grafica_1 %>% 
   ggplot(aes(x = dias_semana_texto, y = total_accidentes)) + 
   geom_col(fill = "#1b3188") + 
   geom_text(aes(label = textos_columnas), 
@@ -48,12 +50,33 @@ grafica <- datos_grafica_1 %>%
             fontface = "bold") + 
   labs(title = "Total de accidentes por día de la semana", 
        subtitle = "Accidentes correspondientes al año 2023", 
-       caption = "Elaboración propia con datos del ATUS (Accidentes de Trásito Terrestre en Zonas Urbanas y Suburbanas) de INEGI, 2023", 
+       caption = "\nElaboración propia con datos del ATUS (Accidentes de Trásito Terrestre en Zonas Urbanas y Suburbanas) de INEGI, 2023", 
        y = "Total de accidentes", 
        x = NULL) + 
-  scale_y_continuous(expand = expansion(c(0, 0.15)))
-
-grafica
+  scale_y_continuous(expand = expansion(c(0, 0.15))) + 
+    # ggthemes::theme_stata()
+  theme_minimal() + 
+  theme(plot.title = element_text(hjust = 0,
+                                  family = "Arial", 
+                                  face = "bold", 
+                                  size = 18
+                                  ), 
+        plot.subtitle = element_text(color = "gray30", 
+                                  hjust = 0,
+                                  family = "Arial", 
+                                  face = "bold", 
+                                  size = 15
+        ), 
+        plot.caption = element_text(color = "white", 
+                                    hjust = 1,
+                                    family = "Arial", 
+                                    face = "bold", 
+                                    size = 10), 
+        axis.text = element_text(size = 10)
+        )
+  
+  
+colors()
 
 
 grafica <- ggbackground(gg = grafica, 
@@ -61,6 +84,74 @@ grafica <- ggbackground(gg = grafica,
 
 ggsave(filename = "03_visualizaciones/grafica_01.jpeg",
        plot = grafica,
-       width = 10,
-       height = 9)
+       width = 9,
+       height = 7)
+
+# Gráfica 2 Series de tiempo ----
+
+
+datos_grafica_2 <- datos %>% 
+  clean_names() %>% 
+  select(mes, contains("herido")) %>% 
+  group_by(mes) %>% 
+  summarise(across(.cols = condherido:totheridos,
+                   .fns = sum)) %>% 
+  pivot_longer(cols = condherido:totheridos, 
+               names_to = "tipo_herido", 
+               values_to = "total_heridos") %>% 
+  filter(tipo_herido != "totheridos") %>% 
+  mutate(tipo_herido_texto = 
+           case_when(tipo_herido == "condherido" ~ "Conductor", 
+                     tipo_herido == "ciclherido" ~ "Ciclista",
+                     tipo_herido == "otroherido" ~ "Otro",
+                     tipo_herido == "pasaherido" ~ "Pasajero",
+                     tipo_herido == "peatherido" ~ "Peatón"
+                     )) %>% 
+  filter(tipo_herido_texto != "Otro")
+  
+vector_colores <- c("Conductor" = "purple",
+                    "Ciclista" = "red",
+                    "Otro" = "yellow",
+                    "Pasajero" = "green",
+                    "Peatón" = "black")  
+
+datos_grafica_2 %>% 
+  ggplot(aes(x = factor(mes), 
+             y = total_heridos, 
+             color = tipo_herido_texto, 
+             group = tipo_herido_texto)) + 
+  geom_line(linewidth = 3) + 
+  labs(title = "Total de heridos, por tipo y mes del año", 
+       subtitle = "Accidentes correspondientes al año 2023", 
+       caption = "Elaboración propia con datos del ATUS (Accidentes de Tránsito Terrestre en Zonas Urbanas y Suburbanas) de INEGI, 2023", 
+       y = "Total de accidentes", 
+       x = "Mes de año", 
+       color = "Tipo de herido: ") + 
+  
+  scale_color_manual(values = vector_colores) + 
+  
+  theme_minimal() + 
+  theme(plot.title = element_text(hjust = 0,
+                                  family = "Arial", 
+                                  face = "bold", 
+                                  size = 18
+  ), 
+  plot.subtitle = element_text(color = "gray30", 
+                               hjust = 0,
+                               family = "Arial", 
+                               face = "bold", 
+                               size = 15
+  ), 
+  plot.caption = element_text(color = "white", 
+                              hjust = 1,
+                              family = "Arial", 
+                              face = "bold", 
+                              size = 10), 
+  axis.text = element_text(size = 10)
+  )
+
+
+
+
+
 
